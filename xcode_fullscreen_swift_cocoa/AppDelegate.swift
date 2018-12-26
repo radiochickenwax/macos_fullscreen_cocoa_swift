de616a79ef4254dbb4b52ce69472d5f4b2bfeb83
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2018-12-26 16:51:58 -0700

    Initial Commit

---
+++ xcode_fullscreen_swift_cocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode_fullscreen_swift_cocoa
+//
+//  Created by Mac User on 12/26/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode_fullscreen_swift_cocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode_fullscreen_swift_cocoa
+//
+//  Created by Mac User on 12/26/18.
+//  Copyright © 2018 Mac User. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
