aadb90bb9c1fef7152c5a2f16a770a084cf0768f
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2018-12-26 17:55:22 -0700

    a couple different fullscreen options in viewcontroller.swift - but this still doesn't work for multiple monitors - and it doesn't let you close the fullscreen window either...

--- xcode_fullscreen_swift_cocoa/ViewController.swift
+++ xcode_fullscreen_swift_cocoa/ViewController.swift
@@ -22,6 +22,20 @@ class ViewController: NSViewController {
         }
     }
 
-
+    // https://stackoverflow.com/questions/38164793/mac-os-xcode-swift-2-2-fullscreen-mode
+    /*
+     // this works - but only for a single monitor
+    override func viewDidAppear() {
+        view.window?.toggleFullScreen(self)
+    }
+     */
+    
+    // this doesn't show the red/yellow/green window controls
+    override func viewDidAppear() {
+        let presOptions: NSApplication.PresentationOptions = [.fullScreen, .autoHideMenuBar]
+        let optionsDictionary = [NSView.FullScreenModeOptionKey.fullScreenModeApplicationPresentationOptions: presOptions]
+        view.enterFullScreenMode(NSScreen.main!, withOptions: optionsDictionary)
+        view.wantsLayer = true
+    }
 }
 
